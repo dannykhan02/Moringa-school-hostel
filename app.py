be793b56bcfaebf08dcf9e08184adb063c0e2c92
alex.wambugu@student.moringaschool.com <wambugumaigua@DESKTOP-F32N3MJ>
2024-07-30 14:05:24 -0700

    Add Reviews

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
 from flask_migrate import Migrate
-
+from flask_jwt_extended import JWTManager
+from model import db  
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key' 
 
-db = SQLAlchemy(app)
+api = Api(app)
+db.init_app(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
+from Reviews import ReviewListResource, ReviewResource, AccommodationReviewResource
+
+
+api.add_resource(ReviewListResource, '/reviews')
+api.add_resource(ReviewResource, '/reviews/<int:id>')
+api.add_resource(AccommodationReviewResource, '/accommodations/<int:accommodation_id>/reviews')
 
 if __name__ == "__main__":
     app.run(debug=True)
