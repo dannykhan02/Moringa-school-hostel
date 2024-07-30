42437960012eb1174545fdbac9de83afb6dae980
Edgar <edgarasige@gmail.com>
2024-07-30 23:53:52 +0300

    fix error in app.py

--- app.py
+++ app.py
@@ -3,25 +3,21 @@ from flask_restful import Api
 from flask_migrate import Migrate
 from model import db
 from accommodation import AccommodationResource, AccommodationAmenityResource
+from Reviews import ReviewListResource, ReviewResource, AccommodationReviewResource
+from bookings import BookingResource
 from amenity import AmenityResource
 
 app = Flask(__name__)
 
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key' 
-
-
+app.config['JWT_SECRET_KEY'] = '' 
 
 db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
-
-from Reviews import ReviewListResource, ReviewResource, AccommodationReviewResource
-
+api.add_resource(BookingResource, '/booking', '/booking/<int:id>')
 
 api.add_resource(ReviewListResource, '/reviews')
 api.add_resource(ReviewResource, '/reviews/<int:id>')
