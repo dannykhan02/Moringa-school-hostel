e40e56665a062b097cc547b79c12b47865f8efe7
dannykhan02 <dannykhan614@gmail.com>
2024-07-30 15:03:19 +0300

    Add accommodation and amenity routes

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
-from bookings import BookingResource, HomeResource
 from model import db
+from accommodation import AccommodationResource, AccommodationAmenityResource
+from amenity import AmenityResource
 
 app = Flask(__name__)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-api = Api(app)
+
 db.init_app(app)
 migrate = Migrate(app, db)
-jwt = JWTManager(app)
+api = Api(app)
 
-api.add_resource(BookingResource, '/booking', '/booking/<int:id>')
-api.add_resource(HomeResource, '/home')
 
-if __name__ == "__main__":
+api.add_resource(AccommodationResource, '/accommodations', '/accommodations/<int:id>')
+api.add_resource(AccommodationAmenityResource, '/accommodations/<int:accommodation_id>/amenities')
+api.add_resource(AmenityResource, '/amenities', '/amenities/<int:id>')
+
+if __name__ == '__main__':
     app.run(debug=True)
