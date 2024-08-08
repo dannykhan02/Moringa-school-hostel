2e804b3c478a51c6594bfa62cdc56d7e184918de
Wambugumaigua <alex.wambugu@student.moringaschool.com>
2024-08-07 20:17:11 -0700

    Add tests for GET method of AmenityResource

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-{
-    "title": "Test Accommodation",
-    "description": "Test Description",
-    "location": "Test Location",
-    "price_per_night": 200,
-    "number_of_rooms": 20,
-    "number_of_students": 5
-}
+[
+    {
+        "name": "New Amenity",
+        "description": "New Description"
+    }
+]
