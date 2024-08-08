a3200c55889c981f3edf677e9ad97499f0d9342e
Wambugumaigua <alex.wambugu@student.moringaschool.com>
2024-08-07 20:02:01 -0700

    Add test for POST accommodation endpoint

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-{
-    "title": "Test Accommodation",
-    "description": "Test Description",
-    "location": "Test Location",
-    "price_per_night": 100,
-    "number_of_rooms": 10,
-    "number_of_students": 5
-}
+    {
+        title='Test Accommodation',
+        description='Test Description',
+        location='Test Location',
+        price_per_night=100,
+        number_of_rooms=10,
+        number_of_students=5
+    }
