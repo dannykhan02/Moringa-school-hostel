9a5502ae0e7e17e706627d61e9367cc55bf9bdc8
Wambugumaigua <alex.wambugu@student.moringaschool.com>
2024-08-07 20:12:02 -0700

    Add test for DELETE accommodation endpoint

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
     "title": "Test Accommodation",
     "description": "Test Description",
     "location": "Test Location",
-    "price_per_night": 100,
-    "number_of_rooms": 10,
+    "price_per_night": 200,
+    "number_of_rooms": 20,
     "number_of_students": 5
 }
