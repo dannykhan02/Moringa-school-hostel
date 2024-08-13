32408b7827de6995450917aa9a58efcdeb69e7cb
Collins <jbcollins254@gmail.com>
2024-08-13 15:54:15 +0300

    updates student table

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Student(db.Model):
     last_name = db.Column(db.String(80), nullable=False)
     gender = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
     bookings = db.relationship('Booking', backref='student', lazy=True, cascade="all, delete-orphan", passive_deletes=True)
     reviews = db.relationship('Review', backref='student', lazy=True, cascade="all, delete-orphan", passive_deletes=True)
     student_amenities = db.relationship('StudentAmenity', back_populates='student', cascade="all, delete-orphan", passive_deletes=True)
